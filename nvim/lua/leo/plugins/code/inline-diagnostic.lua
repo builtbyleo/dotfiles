dee2bbb17ddb65ef2adaee2bf01b0f6370fedf8c
Leo <leonard.paturel@openzeppelin.com>
2026-02-26 16:17:18 +0100

    feat: show source inline diagnostic

--- nvim/lua/leo/plugins/code/inline-diagnostic.lua
+++ nvim/lua/leo/plugins/code/inline-diagnostic.lua
@@ -4,6 +4,10 @@ return {
 	priority = 1000,
 	opts = {
 		options = {
+			show_source = {
+				enabled = true,
+				if_many = true,
+			},
 			multilines = {
 				enabled = true,
 				always_show = true,
