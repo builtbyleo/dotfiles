64153ebdf34fb1c30a02ea4eb14129a058033bf5
Leo <leonard.paturel@openzeppelin.com>
2026-02-26 16:17:31 +0100

    ref: experiment with removing nvim lint

--- nvim/lua/leo/plugins/code/linter.lua
+++ nvim/lua/leo/plugins/code/linter.lua
@@ -1,24 +1,24 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local lint = require("lint")
-
-		lint.linters_by_ft = {
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			python = { "ruff" },
-			solidity = { "solhint" },
-		}
-		vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
-			group = vim.api.nvim_create_augroup("lint", { clear = true }),
-			callback = function()
-				lint.try_lint()
-			end,
-		})
-
-		vim.keymap.set("n", "<leader>ll", function()
-			lint.try_lint()
-		end, { desc = "Trigger linting for current file" })
-	end,
+	-- "mfussenegger/nvim-lint",
+	-- event = { "BufReadPre", "BufNewFile" },
+	-- config = function()
+	-- 	local lint = require("lint")
+	--
+	-- 	lint.linters_by_ft = {
+	-- 		javascriptreact = { "eslint_d" },
+	-- 		typescriptreact = { "eslint_d" },
+	-- 		python = { "ruff" },
+	-- 		solidity = { "solhint" },
+	-- 	}
+	-- 	vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
+	-- 		group = vim.api.nvim_create_augroup("lint", { clear = true }),
+	-- 		callback = function()
+	-- 			lint.try_lint()
+	-- 		end,
+	-- 	})
+	--
+	-- 	vim.keymap.set("n", "<leader>ll", function()
+	-- 		lint.try_lint()
+	-- 	end, { desc = "Trigger linting for current file" })
+	-- end,
 }
