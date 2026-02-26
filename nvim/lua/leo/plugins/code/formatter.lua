dc3269d1f17afb5a8e8bad9ff6d1fe4b8c98a66d
Leo <leonard.paturel@openzeppelin.com>
2026-02-26 16:16:52 +0100

    feat: use biome

--- nvim/lua/leo/plugins/code/formatter.lua
+++ nvim/lua/leo/plugins/code/formatter.lua
@@ -6,11 +6,11 @@ return {
 		local conform = require("conform")
 		conform.setup({
 			formatters_by_ft = {
-				javascript = { "prettier" },
+				javascript = { "biome" },
 				solidity = { "forge_fmt" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
+				typescript = { "biome" },
+				javascriptreact = { "biome" },
+				typescriptreact = { "biome" },
 				css = { "prettier" },
 				html = { "prettier" },
 				json = { "prettier" },
@@ -18,15 +18,15 @@ return {
 				lua = { "stylua" },
 				python = { "ruff" },
 			},
-		formatters = {
-			forge_fmt = {
-				command = vim.fn.exepath("forge") ~= "" and vim.fn.exepath("forge") or "forge",
-				args = { "fmt" },
-				stdin = false,
-				cwd = require("conform.util").root_file({ "foundry.toml" }),
-				require_cwd = true,
+			formatters = {
+				forge_fmt = {
+					command = vim.fn.exepath("forge") ~= "" and vim.fn.exepath("forge") or "forge",
+					args = { "fmt" },
+					stdin = false,
+					cwd = require("conform.util").root_file({ "foundry.toml" }),
+					require_cwd = true,
+				},
 			},
-		},
 			format_on_save = {
 				lsp_fallback = true,
 				async = false,
