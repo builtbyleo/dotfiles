d2c2cab94d30929f68a1488640fe20741681ba8d
builtbyleo <leonardpaturel@proton.me>
2026-06-13 17:37:21 +0200

    feat: update keybind grr to not overwrite others

--- nvim/.config/nvim/lua/leo/plugins/navigation/snacks.picker.lua
+++ nvim/.config/nvim/lua/leo/plugins/navigation/snacks.picker.lua
@@ -82,7 +82,7 @@ return {
             { "<leader>fd", function() Snacks.picker.diagnostics_buffer() end,   desc = "Buffer Diagnostics" },
             { "gd",         function() Snacks.picker.lsp_definitions() end,      desc = "Goto Definition" },
             { "gD",         function() Snacks.picker.lsp_declarations() end,     desc = "Goto Declaration" },
-            { "gr",         function() Snacks.picker.lsp_references() end,       nowait = true, desc = "References" },
+            { "grr",        function() Snacks.picker.lsp_references() end,       nowait = true, desc = "References" },
             { "gI",         function() Snacks.picker.lsp_implementations() end,  desc = "Goto Implementation" },
             { "gy",         function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
             { "<leader>sk", function() Snacks.picker.keymaps() end,              desc = "Keymaps" },
