e000e6885d6c4e113a2f7ac733994740c88e92eb
builtbyleo <leonardpaturel@proton.me>
2026-07-01 11:08:08 +0200

    feat: remove highlight colours

--- nvim/.config/nvim/lua/leo/plugins/utilities/highlight-colours.lua
+++
@@ -1,8 +0,0 @@
-return {
-	-- "brenoprata10/nvim-highlight-colors",
-	-- event = "BufRead",
-	-- opts = {
-	-- 	enable_tailwind = true,
-	-- 	exclude_filetypes = { "js", "ts", "json", "md", "toml", "sol", "rs" },
-	-- },
-}
