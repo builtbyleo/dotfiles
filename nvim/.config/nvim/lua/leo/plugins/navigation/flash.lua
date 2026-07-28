a2db7b47c3a74e16e0af7acda513fcc98ddc85b2
builtbyleo <leonardpaturel@proton.me>
2026-07-28 11:50:37 +0200

    ref: remove broken flash mapping

--- nvim/.config/nvim/lua/leo/plugins/navigation/flash.lua
+++ nvim/.config/nvim/lua/leo/plugins/navigation/flash.lua
@@ -10,7 +10,6 @@ return {
   -- stylua: ignore
  keys = {
         { "<CR>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        { "<leader><CR>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
         { "<C-space>", mode = { "n", "x", "o" }, function()
           require("flash").treesitter({ actions = { ["<C-space>"] = "next", ["<BS>"] = "prev" }}) end,
         desc = "Treesitter incremental selection" },
