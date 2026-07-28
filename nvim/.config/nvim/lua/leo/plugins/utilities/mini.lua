c22432ddd2d5c8299af19db4ee5e498ac9d26703
builtbyleo <leonardpaturel@proton.me>
2026-07-28 12:00:16 +0200

    ref: move mini clue to its own file

--- nvim/.config/nvim/lua/leo/plugins/utilities/mini.lua
+++ nvim/.config/nvim/lua/leo/plugins/utilities/mini.lua
@@ -20,82 +20,8 @@ return {
 			require("mini.git").setup()
 			require("mini.diff").setup(opts.diff)
 			require("mini.statusline").setup()
-			local miniclue = require("mini.clue")
-			miniclue.setup({
-				triggers = {
-					-- Leader triggers
-					{ mode = { "n", "x" }, keys = "<Leader>" },
-
-					-- `[` and `]` keys
-					{ mode = "n", keys = "[" },
-					{ mode = "n", keys = "]" },
-
-					-- Built-in completion
-					{ mode = "i", keys = "<C-x>" },
-
-					-- `g` key
-					{ mode = { "n", "x" }, keys = "g" },
-
-					-- Marks
-					{ mode = { "n", "x" }, keys = "'" },
-					{ mode = { "n", "x" }, keys = "`" },
-
-					-- Registers
-					{ mode = { "n", "x" }, keys = '"' },
-					{ mode = { "i", "c" }, keys = "<C-r>" },
-
-					-- Window commands
-					{ mode = "n", keys = "<C-w>" },
-
-					-- `z` key
-					{ mode = { "n", "x" }, keys = "z" },
-				},
-
-				clues = {
-					-- Enhance this by adding descriptions for <Leader> mapping groups
-					miniclue.gen_clues.square_brackets(),
-					miniclue.gen_clues.builtin_completion(),
-					miniclue.gen_clues.g(),
-					miniclue.gen_clues.marks(),
-					miniclue.gen_clues.registers(),
-					miniclue.gen_clues.windows(),
-					miniclue.gen_clues.z(),
-				},
-				window = {
-					delay = 500,
-				},
-			})
-			-- mini.ai with custom text objects
-			local ai = require("mini.ai")
-			ai.setup({
-				n_lines = 500,
-				custom_textobjects = {
-					-- treesitter-based
-					o = ai.gen_spec.treesitter({ -- code block
-						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-					}),
-					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
-					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
-
-					-- pattern-based
-					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
-					d = { "%f[%d]%d+" }, -- digits
-					e = { -- word with case (camelCase, snake_case, etc.)
-						{
-							"%u[%l%d]+%f[^%l%d]",
-							"%f[%S][%l%d]+%f[^%l%d]",
-							"%f[%P][%l%d]+%f[^%l%d]",
-							"^[%l%d]+%f[^%l%d]",
-						},
-						"^().*()$",
-					},
-
-					-- function calls
-					u = ai.gen_spec.function_call(), -- function call including dot (e.g., foo.bar())
-					U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot
-				},
-			})
+			require("mini.clue").setup(opts.clue)
+			require("mini.ai").setup(opts.ai)
 		end,
 	},
 }
