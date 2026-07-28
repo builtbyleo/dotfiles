db3e1239c005b1dd3a6ad857f930161d0351e358
builtbyleo <leonardpaturel@proton.me>
2026-07-28 11:59:59 +0200

    ref: move mini ai to treesitter

--- nvim/.config/nvim/lua/leo/plugins/code/treesitter.lua
+++ nvim/.config/nvim/lua/leo/plugins/code/treesitter.lua
@@ -74,4 +74,39 @@ return {
 			},
 		},
 	},
+	{
+		"nvim-mini/mini.nvim",
+		opts = function(_, opts)
+			local ai = require("mini.ai")
+			opts.ai = {
+				n_lines = 500,
+				custom_textobjects = {
+					-- treesitter-based
+					o = ai.gen_spec.treesitter({ -- code block
+						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+					}),
+					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+
+					-- pattern-based
+					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+					d = { "%f[%d]%d+" }, -- digits
+					e = { -- word with case (camelCase, snake_case, etc.)
+						{
+							"%u[%l%d]+%f[^%l%d]",
+							"%f[%S][%l%d]+%f[^%l%d]",
+							"%f[%P][%l%d]+%f[^%l%d]",
+							"^[%l%d]+%f[^%l%d]",
+						},
+						"^().*()$",
+					},
+
+					-- function calls
+					u = ai.gen_spec.function_call(), -- function call including dot (e.g., foo.bar())
+					U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot
+				},
+			}
+		end,
+	},
 }
